33274462ffa26c91abf9a24b00e6dff6807395f6
Prakash Anand <ppa58514@gmail.com>
2020-08-16 00:18:00 +0530

    Login sign up cart done

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -7,6 +7,12 @@ function authController() {
       res.render('auth/login')
     },
     postLogin(req,res,next){
+      const {email,password}=req.body
+      if(!email || !password)
+      {
+        req.flash('error','All fields are required')
+        return res.redirect('/login')
+      }
       passport.authenticate('local',(err,user,info) =>{
         if(err) {
           req.flash('error',info.message)
